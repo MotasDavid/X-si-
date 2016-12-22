bd39663f7a881c0b763a8e7f1ef3c3cb0c3db1fd
Motas David <motas_dvd@yahoo.com>
2016-12-22 10:41:21 +0200

    Initial Commit

---
+++ tic tac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tic tac
+//
+//  Created by motas david on 22.12.2016.
+//  Copyright © 2016 motas david. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
