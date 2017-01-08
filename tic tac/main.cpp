5c245c3bc2033b2188f41cc9238e7425f223e709
Motas David <motas_dvd@yahoo.com>
2017-01-09 00:33:53 +0200

    update player vs co ( Player vs player)

--- tic tac/main.cpp
+++ tic tac/main.cpp
@@ -113,11 +113,65 @@ int main()
                         _getche();
                         tabla(matrice);
                     }
+                    //jucator 1
                     
-                }
+                    else if(jucator == 1)
+                    {
+                        cout<< " Este randul tau "<< nume << " :\nIntrodu un numar unde vei plasa X: ";
+                        cin >> alegere;
+                        semn = 'X';
+                        if( alegere == 1 && matrice[1] == '1' )
+                            matrice[1] = semn;
+                        else
+                            if( alegere == 2 && matrice[2] == '2')
+                                matrice[2] = semn;
+                        else
+                            if( alegere == 3 && matrice[3] == '3')
+                                matrice[3] = semn;
+                        else
+                            if( alegere == 4 && matrice [4] == '4')
+                                matrice[4] = semn;
+                        else
+                            if( alegere == 5 && matrice[5] == '5')
+                                matrice[5] = semn;
+                        else
+                            if( alegere == 6 && matrice[6] == '6')
+                                matrice[6] = semn;
+                        else
+                            if( alegere == 7 && matrice[7] == '7')
+                                matrice[7] = semn;
+                        else
+                            if( alegere == 8 && matrice[8] == '8')
+                                matrice[8] = semn;
+                        else
+                            if( alegere == 9 && matrice[9] == '9')
+                                matrice[9] = semn;
+                        else
+                        {
+                            cout << "Mutarea este gresita!!!";
+                            jucator--;
+                            _getche();
+                        }
+                        i = verificaCastig(matrice);
+                        jucator++;
+                    }
+                }while ( i == -1);
+                tabla(matrice);
+                if( i == 1)
+                    cout<< " Felicitari! \njucatorul "<< --jucator << " a castigat";
+                else
+                    cout<< "A fost un joc bun! \nRemiza";
+                fflush(stdin);
+                
+                _getche();
+                for ( int i = 1; i <= 9; i++ )
+                    matrice[i] = i + '0';
+                break;
         }
+        
+        _getch();
     }
-    
+    return 0;
 }
 
 void tabla(char matrice[])
